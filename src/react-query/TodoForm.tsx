47db6d3aac7b3715cd353e51ead4315457a45cf0
e-muchasaj <e.muchasaj@7btc.co>
2024-08-28 16:58:28 +0200

    Optimistic Update with Mutations

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,24 +3,42 @@ import { useRef } from 'react';
 import { Todo } from './hooks/useTodos';
 import axios from 'axios';
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
-    mutationFn: (todo: Todo) => axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo).then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
-      //APPROACH: Invalidate the cache to refetch the data
-      //queryClient.invalidateQueries({ queryKey: ['todos'] });
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+    onMutate: (newTodo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
 
       //APPROACH: Update the cache with the new data without refetching
-      queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]);
+      queryClient.setQueryData<Todo[]>(['todos'], todos => [newTodo, ...(todos || [])]);
 
       if (ref.current) {
         ref.current.value = '';
       }
+
+      return { previousTodos };
     },
-    onError(error, variables) {
-      console.error('error:', error);
-      console.error('variables:', variables);
+    mutationFn: (todo: Todo) => axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo).then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      //APPROACH: Invalidate the cache to refetch the data
+      //queryClient.invalidateQueries({ queryKey: ['todos'] });
+      queryClient.setQueryData<Todo[]>(['todos'], todos =>
+        todos?.map(todo =>
+          todo.id === newTodo.id ? savedTodo : todo
+        )
+      );
+    },
+    onError(error, newTodo, context) {
+      //APPROACH: Revert the cache to the previous state
+      if (!context) {
+        return;
+      }
+
+      queryClient.setQueryData<Todo[]>(['todos'], context.previousTodos);
     },
   });
 
