2179b534e224f18fd42d6cc8144a19a76cdd876f
e-muchasaj <e.muchasaj@7btc.co>
2024-08-28 08:33:35 +0200

    Implemented Infinite Scrolling on PostLists/usePost

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,11 +1,9 @@
+import React from 'react';
 import usePosts from './hooks/usePosts';
-import { useState } from 'react';
-
 
 const PostList = () => {
   const pageSize = 10;
-  const [page, setPage] = useState<number>(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } = usePosts({ pageSize });
 
   if (error) return <p>{error.message}</p>;
 
@@ -15,24 +13,28 @@ const PostList = () => {
     <>
       <h1>Posts</h1>
       <ul className="list-group">
-        {posts?.map((post) => (
+        {/* {data?.pages.map((page) => page.map((post) => (
           <li key={post.id} className="list-group-item">
             {post.title}
           </li>
-        ))}
+        )))
+        } */}
+        {
+          data?.pages.map((page, index) =>
+            <React.Fragment key={index}>
+              {page.map((post) =>
+                <li key={post.id} className="list-group-item">{post.title}</li>
+              )}
+            </React.Fragment>
+          )
+        }
       </ul>
       <button
-        className="btn btn-primary my-3 mx-2"
-        onClick={() => setPage((page) => page - 1)}
-        disabled={page === 1}
-      >
-        Previous
-      </button>
-      <button
         className="btn btn-primary my-3 ms-2"
-        onClick={() => setPage((page) => page + 1)}
+        onClick={() => fetchNextPage()}
+        disabled={isFetchingNextPage}
       >
-        Next
+        {isFetchingNextPage ? 'Loading...' : 'Load more'}
       </button>
     </>
   );
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { keepPreviousData, useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 interface Post {
@@ -9,23 +9,26 @@ interface Post {
 }
 
 interface PostQuery {
-    page: number;
     pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-    return useQuery<Post[], Error>({
+    return useInfiniteQuery<Post[], Error>({
         queryKey: ['posts', query],
-        queryFn: () => axios
+        queryFn: ({ pageParam = 1 }) => axios
             .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
                 params: {
-                    _start: (query.page - 1) * query.pageSize,
+                    _start: (pageParam - 1) * query.pageSize,
                     _limit: query.pageSize,
                 }
             })
             .then((res) => res.data),
-        staleTime: 1 * 60 * 1000, //1m
+        initialPageParam: 1,
+        staleTime: 1 * 60 * 1000, //1min
         placeholderData: keepPreviousData,
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length > 0 ? allPages.length + 1 : undefined;
+        }
     });
 };
 
