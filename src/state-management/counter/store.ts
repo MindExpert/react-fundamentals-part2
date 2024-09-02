4ad5d8cf604a8e0a7012fd43fd79ba5f67c44e8e
e-muchasaj <e.muchasaj@7btc.co>
2024-09-02 08:37:23 +0200

    Inspecting State with Zustand dev tools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 interface CounterStore {
     counter: number;
@@ -13,5 +14,9 @@ const useCounterStore = create<CounterStore>(set => ({
     decrement: () => set(store => ({ counter: store.counter - 1 })),
     reset: () => set(() => ({ counter: 0 })),
 }));
+
+if (process.env.NODE_ENV === 'development') {
+    mountStoreDevtool('CounterStore', useCounterStore);
+}
 
 export default useCounterStore;
